a47816d9ce5587f2e073f5efd6ac4127de0be830
rodrigodiasb <62857786+rodrigodiasb@users.noreply.github.com>
2025-06-19 18:55:51 -0400

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+const perguntas = [
+  {
+    categoria: "História",
+    respostas: ["Albert Einstein"],
+    dicas: [
+      "Nasci na Europa em 1879",
+      "Trabalhei com física teórica",
+      "Minha fórmula famosa é E=mc²",
+      "Recebi o Nobel em 1921",
+      "Meu nome é Albert _______"
+    ]
+  },
+  {
+    categoria: "Geografia",
+    respostas: ["Brasil"],
+    dicas: [
+      "Tenho 26 estados e um Distrito Federal",
+      "Minha capital é Brasília",
+      "Sou o maior país da América do Sul",
+      "Minha língua oficial é o português",
+      "Meu nome começa com B"
+    ]
+  }
+];
